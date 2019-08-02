7aaac9575c3e3130d15914cb9b8d21ebfbb31f04
Rhys <godjdc@gmail.com>
2019-08-02 16:23:51 +0800

    update

--- coding-dojo-round-1/src/main/java/tw/com/softleader/advancedtraining/codingdojoround1/CodingDojo/dao/CodingDojoDao.java
+++ coding-dojo-round-1/src/main/java/tw/com/softleader/advancedtraining/codingdojoround1/CodingDojo/dao/CodingDojoDao.java
@@ -1,7 +1,8 @@
 package tw.com.softleader.advancedtraining.codingdojoround1.CodingDojo.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import tw.com.softleader.advancedtraining.codingdojoround1.CodingDojo.entity.CodingDojo;
 
-public interface CodingDojoDao extends   CrudRepository<CodingDojoDao, Long > {
+public interface CodingDojoDao extends JpaRepository<CodingDojo, Long > {
 
 }
--- coding-dojo-round-1/src/main/java/tw/com/softleader/advancedtraining/codingdojoround1/CodingDojo/entity/CodingDojo.java
+++ coding-dojo-round-1/src/main/java/tw/com/softleader/advancedtraining/codingdojoround1/CodingDojo/entity/CodingDojo.java
@@ -4,11 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @ToString
+@Entity
 public class CodingDojo {
 
+  @Id
+  @GeneratedValue
   private Long id;
 
   private Long Seq;
