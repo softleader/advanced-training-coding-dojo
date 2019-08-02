c2f1e24198beefbc5b7271a700b80f92d2a2ce8f
sam.shih <sam.shih@softleader.com.tw>
2019-08-02 17:41:46 +0800

    Sam

--- coding-dojo-round-1/src/main/java/tw/com/softleader/advancedtraining/codingdojoround1/CodingDojo/web/CodingDojoController.java
+++ coding-dojo-round-1/src/main/java/tw/com/softleader/advancedtraining/codingdojoround1/CodingDojo/web/CodingDojoController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tw.com.softleader.advancedtraining.codingdojoround1.CodingDojo.entity.CodingDojo;
 import tw.com.softleader.advancedtraining.codingdojoround1.CodingDojo.service.CodingDojoService;
@@ -14,8 +15,7 @@ import java.util.List;
 
 @RestController
 public class CodingDojoController {
-  @Autowired
-  CodingDojoService service;
+  @Autowired CodingDojoService service;
 
   @GetMapping("/getall")
   public List<CodingDojo> getAll(Model model) {
@@ -25,9 +25,14 @@ public class CodingDojoController {
   }
 
   @PostMapping("/save")
-  public String saveMessage(@RequestBody CodingDojo form, HttpRequest httpRequest) {
-    form.setIp(httpRequest.getHeaders().toString());
-    service.save(form);
+  public String saveMessage(
+      @RequestParam("user") String user,
+      @RequestParam("content") String content,
+      HttpRequest httpRequest) {
+//    form.setIp(httpRequest.getHeaders().toString());
+    CodingDojo codingDojo = new CodingDojo();
+    codingDojo.setUser(user);
+//    service.save(form);
     return "/";
   }
 }
