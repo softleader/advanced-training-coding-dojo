4dc6f012bd3d6afca1293d2ca42246e8f9457d5c
Alan.wei <wyta129@gmail.com>
2019-08-02 16:33:24 +0800

    update

---
+++ coding-dojo-round-1/src/main/java/tw/com/softleader/advancedtraining/codingdojoround1/CodingDojo/service/CodingDojoService.java
@@ -0,0 +1,5 @@
+package tw.com.softleader.advancedtraining.codingdojoround1.CodingDojo.service;
+
+public interface CodingDojoService {
+
+}
---
+++ coding-dojo-round-1/src/main/java/tw/com/softleader/advancedtraining/codingdojoround1/CodingDojo/service/impl/CodingDojoServiceImpl.java
@@ -0,0 +1,29 @@
+package tw.com.softleader.advancedtraining.codingdojoround1.CodingDojo.service.impl;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tw.com.softleader.advancedtraining.codingdojoround1.CodingDojo.dao.CodingDojoDao;
+import tw.com.softleader.advancedtraining.codingdojoround1.CodingDojo.entity.CodingDojo;
+import tw.com.softleader.advancedtraining.codingdojoround1.CodingDojo.service.CodingDojoService;
+
+@Service
+@Transactional
+public class CodingDojoServiceImpl implements CodingDojoService {
+	
+	@Autowired
+	private CodingDojoDao dao;
+
+	public CodingDojoDao getDao() {
+		return dao;
+	}
+	
+	public CodingDojo save(CodingDojo codingDojo) {
+		
+		return dao.save(codingDojo);
+	}
+	
+
+}
