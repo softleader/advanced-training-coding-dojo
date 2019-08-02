7c9334aafc08100a3c0d3c8295de12b44455633c
Rhys <godjdc@gmail.com>
2019-08-02 15:36:25 +0800

    init

---
+++ coding-dojo-round-1/src/main/java/tw/com/softleader/advancedtraining/codingdojoround1/CodingDojoRound1Application.java
@@ -0,0 +1,13 @@
+package tw.com.softleader.advancedtraining.codingdojoround1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingDojoRound1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingDojoRound1Application.class, args);
+	}
+
+}
---
+++ coding-dojo-round-1/src/main/java/tw/com/softleader/advancedtraining/codingdojoround1/config/WebMvcConfig.java
@@ -0,0 +1,13 @@
+package tw.com.softleader.advancedtraining.codingdojoround1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Override public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/").setViewName("index");
+  }
+}
