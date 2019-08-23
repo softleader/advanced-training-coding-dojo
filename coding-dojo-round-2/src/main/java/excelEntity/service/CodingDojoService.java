46b238f76c59bef6bee2a7f00dda8d5dbbd287d1
Jack Dai <jack.dai@softleader.com.tw>
2019-08-23 16:53:22 +0800

    Jack

--- coding-dojo-round-2/src/main/java/excelEntity/service/CodingDojoService.java
+++ coding-dojo-round-2/src/main/java/excelEntity/service/CodingDojoService.java
@@ -1,8 +1,13 @@
 package excelEntity.service;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.apache.commons.compress.utils.Lists;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.sl.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,11 +18,23 @@ public class CodingDojoService {
 
 	public List<ExcelUpload> dataUpload(MultipartFile file) {
 		List<ExcelUpload> excelUploadList = Lists.newArrayList();
-		
-		Workbook workbook = null;
-		Sheet insuredSheet = null;
+
+		try(InputStream in = file.getInputStream()) {
+			HSSFWorkbook wb = new HSSFWorkbook(in);
+			HSSFSheet sheet = wb.getSheetAt(0);
+			for (int i=0 ; i < sheet.getLastRowNum() ; ++i) {
+				HSSFRow row = sheet.getRow(i);
+				row.getCell(0).getStringCellValue();
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 
 
 		return excelUploadList;
 	}
+
+
 }
