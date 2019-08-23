ed19bbd51204bfc3ca6e7a8a66af1fd662c75759
Rhys <godjdc@gmail.com>
2019-08-23 16:00:57 +0800

    init round2

---
+++ coding-dojo-round-2/src/main/java/tw/com/softleader/advancedtraining/codingdojoround2/CodingDojoRound2Application.java
@@ -0,0 +1,13 @@
+package tw.com.softleader.advancedtraining.codingdojoround2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingDojoRound2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingDojoRound2Application.class, args);
+	}
+
+}
