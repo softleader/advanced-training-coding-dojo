784a31d07f5dff59db60f148386910d468895689
Sarah Chou <dunk800034@gmail.com>
2019-08-23 16:33:06 +0800

    Sarah

---
+++ coding-dojo-round-2/src/main/java/excelEntity/entity/ExcelUpload.java
@@ -0,0 +1,30 @@
+package excelEntity.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class ExcelUpload {
+
+
+  //文字    整數    小數    時間    日期    日期時間
+  private Long id;
+
+  private String word;
+
+  private Integer number;
+
+  private Double doubleNumber;
+
+  private LocalDateTime time = LocalDateTime.now();
+
+  private LocalDateTime timeWithMD = LocalDateTime.now();
+
+  private LocalDateTime timeWithMDHS = LocalDateTime.now();
+
+}
